842f358159def4f1f7e11ecd55675b01390279c1
defrgtyuiomp0 <113428666+defrgtyuiomp0@users.noreply.github.com>
2022-09-21 11:54:31 +0300

    Update Laba5.1.py

--- Laba5.1.py
+++ Laba5.1.py
@@ -6,4 +6,4 @@ elif 0.19 < x < 5.2:
     y = math.cos(x)+(1/math.tan(x)+math.sqrt(math.fabs(x-7.84)))
 else:
     y = math.sin(math.fabs(x-0.7))+3.33*math.pow(2,x)
-print('Вшдповідь: ',y)
+print('Відповідь: ',y)
